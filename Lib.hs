c013886f5ceb4cfea20c7db2922b12bc418eca77
isabellamastrangelo <china.mastrangelo@gmail.com>
2025-05-29 17:40:28 -0300

    Punto 2

--- Lib.hs
+++ Lib.hs
@@ -41,3 +41,27 @@ mayorSegun f a b
 ----------------------------------------------
 ---- Resolución del ejercicio
 ----------------------------------------------
+
+
+ 
+data NombrePalos = Putter | Madera | Hierros deriving (Eq,Show)
+
+{-data Palitos = UnPalo {
+  tipo :: NombrePalos, 
+  habilidad ::  Habilidad, 
+} deriving (Eq,Show)-}
+
+data RangoHierro = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 deriving(Eq,Show)
+
+palo :: NombrePalos -> Habilidad -> RangoHierro -> Tiro
+palo Putter hab _ = UnTiro{velocidad = 10, precision = precisionJugador hab * 2, altura = 0 }
+palo Madera hab _ = UnTiro{velocidad = 100, precision = precisionJugador hab / 2, altura = 5 }
+palo Hierro hab n = UnTiro {velocidad = fuerzaJugador hab * n , precision = precisionJugador hab * n, altura = max (n-3) 0 }
+
+palos :: [NombrePalos]
+palos = [Putter, Madera, Hierro]
+
+
+golpe :: UnJugador -> NombrePalos -> RangoHierro -> Tiro
+golpe jugador paloAUsar = palo paloAUsar (habilidad jugador)
+
