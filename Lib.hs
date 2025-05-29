5abf333c38f962ca1f5f18e5b8e335e10eb007b6
isabellamastrangelo <china.mastrangelo@gmail.com>
2025-05-29 19:58:37 -0300

    3 y medio

--- Lib.hs
+++ Lib.hs
@@ -44,24 +44,33 @@ mayorSegun f a b
 
 
  
-data NombrePalos = Putter | Madera | Hierros deriving (Eq,Show)
+data NombrePalos = Putter | Madera | Hierro Number deriving (Eq,Show)
 
 {-data Palitos = UnPalo {
   tipo :: NombrePalos, 
   habilidad ::  Habilidad, 
 } deriving (Eq,Show)-}
 
-data RangoHierro = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 deriving(Eq,Show)
 
-palo :: NombrePalos -> Habilidad -> RangoHierro -> Tiro
-palo Putter hab _ = UnTiro{velocidad = 10, precision = precisionJugador hab * 2, altura = 0 }
-palo Madera hab _ = UnTiro{velocidad = 100, precision = precisionJugador hab / 2, altura = 5 }
-palo Hierro hab n = UnTiro {velocidad = fuerzaJugador hab * n , precision = precisionJugador hab * n, altura = max (n-3) 0 }
+
+palo :: NombrePalos -> Habilidad -> Tiro
+palo Putter hab = UnTiro{velocidad = 10, precision = precisionJugador hab * 2, altura = 0 }
+palo Madera hab = UnTiro{velocidad = 100, precision = precisionJugador hab / 2, altura = 5 }
+palo (Hierro n) hab = UnTiro {velocidad = fuerzaJugador hab * n , precision = precisionJugador hab * n, altura = max (n-3) 0 }
 
 palos :: [NombrePalos]
-palos = [Putter, Madera, Hierro]
+palos = [Putter, Madera] ++ 
 
 
 golpe :: UnJugador -> NombrePalos -> RangoHierro -> Tiro
 golpe jugador paloAUsar = palo paloAUsar (habilidad jugador)
 
+data TipoObstaculo = Tunel | Laguna | Hoyo deriving (Eq,Show)
+
+obstaculo :: TipoObstaculo -> Tiro -> Tiro
+obstaculo Tunel tiro 
+  | precision tiro > 90 = UnTiro {velocidad = velocidad tiro * 2, precision = 100, altura = 0}
+  | otherwise = UnTiro {velocidad = 0, precision = 0, altura = 0}
+obstaculo Laguna tiro
+  | velocidad tiro > 80 && altura tiro < 5 && altura tiro > 1 = 
+  | otherwise = t 
